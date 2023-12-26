0d27cceca09bfc36ca5a5bb5a9544ce84bc886bf
Lex <info@nextlevel.trading>
2023-12-26 15:01:00 +0100

    step2_imgGrid:Debug completed

---
+++ LexMe/LexMe.md
@@ -0,0 +1,17 @@
+//Beispiel: //https://github.com/jide/gpt-video
+//https://www.youtube.com/watch?v=Bf1zaKUMKeQ&t=982s
+
+#0//Project Setup - Vorbereitung Erst-Installation:
+    
+    npx create-next-app@latest --ts --tailwind --eslint
+ 
+ - src/ directory = Yes
+ - App Router = Yes
+ - import alias = NO
+
+
+#1//SDK AI by Vercel//Acces to OpenAI Functoinalitys///Silence-Aware-Recorder for Audio//Media-Recorder for Video//Merge-Images for Image-Merging//
+   
+    npm install ai openai silence-aware-recorder @wmik/use-media-recorder merge-images
+
+#3//To get environment variables: able to access anywhere with "process.env.OPENAI_API_KEY" in api/speechtotext/route.ts => create ".env.local"-file in root directory with "OPENAI_API_KEY=YOUR_API_KEY"
---
+++ LexMe/step2_imgGrid-Debug/src/app/chat.jsx
@@ -0,0 +1,323 @@
+"use client"
+
+import { useId, useEffect, useRef, useState } from "react";
+import { useChat } from "ai/react";
+import useSilenceAwareRecorder from "silence-aware-recorder/react";
+import useMediaRecorder from "@wmik/use-media-recorder";
+import mergeImages from "merge-images";
+import { split } from "postcss/lib/list";
+
+const INTERVAL = 250;
+const IMAGE_WIDTH = 512;
+const IMAGE_QUALITY = 0.6;
+const COLUMNS = 4;
+const MAX_SCREENSHOTS = 60;
+const SILENCE_DURATION = 2500;
+const SILENT_THRESHOLD = -30;
+
+const transparentPixel =
+  "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8/wcAAwAB/2lXzAAAACV0RVh0ZGF0ZTpjcmVhdGU9MjAyMy0xMC0xOFQxNTo0MDozMCswMDowMEfahTAAAAAldEVYdGRhdGU6bW9kaWZ5PTIwMjMtMTAtMThUMTU6NDA6MzArMDA6MDBa8cKfAAAAAElFTkSuQmCC";
+
+async function getImageDimensions(src) {
+    return new Promise((resolve, reject) => {
+        const img = new globalThis.Image();
+
+        img.onload = function () {
+            resolve({ 
+                width: this.width, 
+                height: this.height,
+            });
+        };
+
+        img.onerror = function () {
+            reject(new Error("Image failed to load"));
+        };
+
+        img.src = src;
+    });
+}
+
+function base64ToBlob(base64, mimeType) {
+    const byteCharacters = atob(base64.split(",")[1]);
+    const byteNumbers = new Array(byteCharacters.length);
+    for (let i = 0; i < byteCharacters.length; i++) {
+        byteNumbers[i] = byteCharacters.charCodeAt(i);
+    }
+    const byteArray = new Uint8Array(byteNumbers);
+    return new Blob([byteArray], { type: mimeType });
+}
+
+async function uploadImageToFreeImageHost(base64Image) {
+    const blob = base64ToBlob(base64Image, "image/jpeg");
+    const formData = new FormData();
+    formData.append("file", blob, "image.jpg");
+
+    const response = await fetch("https://tmpfiles.org/api/v1/upload", {
+        method: "POST",
+        body: formData,
+    });
+
+    const { data } = await response.json();
+
+    return data.url.replace("https://tmpfiles.org/", "https://tmpfiles.org/dl/");
+}
+
+async function imagesGrid({
+    base64Images,
+    columns = COLUMNS,
+    gridImageWidth = IMAGE_WIDTH,
+    quality = IMAGE_QUALITY,
+}) {
+    if (!base64Images.length) {
+        return transparentPixel;
+    }
+
+    const dimensions = await getImageDimensions(base64Images[0]);
+
+    //Calculate the aspect ratio of the first image
+    const aspectRatio = dimensions.width / dimensions.height;
+
+    const gridImageHeight = gridImageWidth / aspectRatio;
+
+    const rows = Math.ceil(base64Images.length / columns);  //Number of rows
+
+    //Prepare the images for merging
+    const imagesWidthCoordinates = base64Images.map((src, index) => ({
+        src,
+        x: (index % columns) * gridImageWidth,
+        y: Math.floor(index / columns) * gridImageHeight,
+    }));
+
+    //Merge images into a single base64 string
+    return await mergeImages(imagesWidthCoordinates, {
+        format: "image/jpeg",
+        quality,
+        width: columns * gridImageWidth,
+        height: rows * gridImageHeight,
+    });
+}
+
+export default function Chat() {
+    const id = useId();
+    const maxVolumeRef = useRef(0);
+    const minVolumeRef = useRef(-100);
+    const isBusy = useRef(false);
+    const screenshotsRef = useRef([]);
+    const videoRef = useRef();
+    const canvasRef = useRef();
+    const [displayDebug, setDisplayDebug] = useState(false);
+    const [isStarted, setIsStarted] = useState(false);
+    const [phase, setPhase] = useState("not inited");
+    const [transcription, setTranscription] = useState("");
+    const [imagesGridUrl, setImagesGridUrl] = useState(null);
+    const [currentVolume, setCurrentVolume] = useState(-50);
+    const [volumePercentage, setVolumePercentage] = useState(0);
+
+    let { liveStream, ...video } = useMediaRecorder({
+        recordScreen: false,
+        blobOptions: { type: "video/webm" },
+        mediaStreamConstraints: { audio: false, video: true},
+    });
+
+    const audio = useSilenceAwareRecorder({
+        onDataAvailable: onSpeech,
+        onVolumeChange: setCurrentVolume,
+        silenceDuration: SILENCE_DURATION,
+        silentThreshold: SILENT_THRESHOLD,
+        minDecibels: -100,
+    });
+
+    function startRecording() {
+        audio.startRecording();
+        video.startRecording();
+
+        setIsStarted(true);
+        setPhase("user: waiting for speech");
+    }
+
+    function stopRecording() {
+        document.location.reload();
+    }
+
+    async function onSpeech(data) {
+        if (isBusy.current) return;
+        
+        isBusy.current = true;
+        audio.stopRecording();
+
+        //send audio to whisper for transcription
+
+        //generate image grid
+        setPhase("user: upload video captures");
+
+        //Keep only the last XXX screenshots
+        screenshotsRef.current = screenshotsRef.current.slice(-MAX_SCREENSHOTS);
+
+        const imageUrl = await imagesGrid({
+            base64Images: screenshotsRef.current,
+        });
+
+        screenshotsRef.current = [];
+
+        const uploadUrl = await uploadImageToFreeImageHost(imageUrl);
+
+        setImagesGridUrl(imageUrl);
+
+        setPhase("user: processing completion");
+    }
+
+    useEffect(() => {
+        if (videoRef.current && liveStream && !videoRef.current.srcObject) {
+            videoRef.current.srcObject = liveStream;
+        }
+    }, [liveStream]);
+
+    //To safe Screenshots every few frames
+    useEffect(() => {
+        const captureFrame = () => {
+            if (video.status === "recording" && audio.isRecording) {
+                const target = {}; 
+                target.Width = IMAGE_WIDTH;
+
+                const videoNode = videoRef.current;
+                const canvasNode = canvasRef.current;
+
+                if (videoNode && canvasNode) {
+                    const context = canvasNode.getContext("2d");
+                    const originalWidth = videoNode.videoWidth;
+                    const originalHeight = videoNode.videoHeight;
+                    const aspectRatio = originalHeight / originalWidth;
+
+                    //Set new width while maintaining aspect ratio
+                    canvasNode.width = target.Width;
+                    canvasNode.height = target.Width * aspectRatio;
+
+                    context.drawImage(
+                        videoNode,
+                        0,
+                        0,
+                        canvasNode.width,
+                        canvasNode.height,
+                    );
+                    //Compress and convert image to JPEG format
+                    const quality = 1 //Adjust the quality as needed, between 0 and 1
+                    const base64Image = canvasNode.toDataURL("image/jpeg", quality);
+
+                    if (base64Image !== "data:,") {
+                        screenshotsRef.current.push(base64Image);
+                    }
+                }
+            }    
+        };
+
+        const intervalId = setInterval(captureFrame, INTERVAL);
+
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, [video.status, audio.isRecording]);
+    
+
+    useEffect(() => {
+        if (!audio.isRecording) {
+            setVolumePercentage(0);
+            return;
+        }
+
+        if (typeof currentVolume === "number" && isFinite(currentVolume)) {
+            if (currentVolume > maxVolumeRef.current)
+                maxVolumeRef.current = currentVolume;
+            if (currentVolume < minVolumeRef.current)
+                minVolumeRef.current = currentVolume;
+
+            if (maxVolumeRef.current !== minVolumeRef.current) {
+                setVolumePercentage(
+                    (currentVolume - minVolumeRef.current) / 
+                    (maxVolumeRef.current - minVolumeRef.current)
+                );
+            }
+        }
+    }, [currentVolume], audio.isRecording);
+
+    return (
+        <>
+            <canvas ref={canvasRef} style={{ display: "none" }} />
+            <div className="antialiased w-screen h-screen p-4 flex flex-col justify-center items-center bg-black">
+                <div className="w-full h-full sm:container sm:h-auto grid grid-rows-[auto_1fr] grid-cols-[1fr] sm:grid-cols-[2fr_1fr] sm:grid-rows-[1fr] justify-content-center bg-black">
+                    <div className="relative">
+                        <video 
+                            ref={videoRef} 
+                            className="h-auto w-full aspect-[4/3] object-cover rounded-[1rem] bg-gray-900"
+                            autoPlay
+                        />
+                        {audio.isRecording ? (
+                        <div className="w-16 h-16 absolute bottom-4 left-4 flex justify-center items-center">
+                            <div
+                            className="w-16 h-16 bg-red-500 opacity-50 rounded-full"
+                            style={{
+                                transform: `scale(${Math.pow(volumePercentage, 4).toFixed(
+                                4
+                                )})`,
+                            }}
+                            ></div>
+                        </div>
+                        ) : (
+                            <div className="w-16 h-16 absolute bottom-4 left-4 flex justify-center items-center cursor-pointer">
+                                <div className="text-5xl text-red-500 opacity-50">❚❚</div>
+                            </div>
+                        )}
+                    </div>
+                </div>
+            </div>
+            <div className="flex flex-wrap justify-center p-4 opacity-50 gap-2">
+            {isStarted ? (
+                <button 
+                    className="px-4 py-2 bg-gray-700 rounded-md disabled:opacity-50"
+                    onClick={stopRecording}
+                >
+                    Stop session
+                    </button>
+                ) : (
+                    <button
+                        className="px-4 py-2 bg-gray-700 rounded-md disabled:opacity-50"
+                        onClick={startRecording}
+                    >
+                        Start session
+                    </button>
+                )}
+                <button
+                    className="px-4 py-2 bg-gray-700 rounded-md disabled:opacity-50"
+                    onClick={() => setDisplayDebug((p) => !p)}
+                >
+                    Debug
+                </button>
+            </div>
+            <div 
+                className={`bg-[rgba(20,20,20,0.8)] backdrop-blur-xl p-8 rounded-sm absolute left-0 top-0 bottom-0 transition-all w-[75vw] sm:w-[33vw] ${
+                displayDebug ? "translate-x-0" : "-translate-x-full"
+                }`}
+            >
+                <div
+                className="absolute z-10 top-4 right-4 opacity-50 cursor-pointer"
+                onClick={() => setDisplayDebug(false)}
+                >
+                ⛌
+                </div>
+                <div className="space-y-8">
+                <div className="space-y-2">
+                    <div className="font-semibold text-white opacity-50">Phase:</div>
+                    <p className="text-white">{phase}</p>
+                </div>
+                <div className="space-y-2">
+                    <div className="font-semibold text-white opacity-50">Captures:</div>
+                    <img
+                    className="object-contain w-full border border-gray-500"
+                    alt="Grid"
+                    src={imagesGridUrl || transparentPixel}
+                    />
+                </div>
+            </div>
+        </div>    
+        </>
+    );
+}
